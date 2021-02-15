66bbc43a4044fdb876a286a39ad8425fb7a1dfae
cliffybar <crowellclifford@utexas.edu>
2021-02-15 16:18:13 -0600

    fixed else statement in the for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -76,30 +76,31 @@ var fight = function(enemyNames) {
     }
 };
 
+// Fight each enemy-robot by looping over them and fighting them one at a time
 for(var i = 0; i < enemyNames.length; i++) {
+    // If player is still alive, keep fighting
     if(playerHealth > 0) {
-        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        // Let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
-        // pick new enemy to fight based on the index of the enemyNames array
+        // Pick new enemy to fight based on the index of the enemyNames array
         var pickedEnemyName = enemyNames[i];
 
-        // reset enemyHealth before starting new fight
+        // Reset enemyHealth before starting new fight
         enemyHealth = 50;
 
-        // use debugger to pause script from running and check what's going on at that moment in the code
-        // debugger;
+        // Use debugger to pause script from running and check what's going on at that moment in the code
+        debugger;
 
-        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        // Pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
     }
+    
+    // If player isn't alive, stop the game
     else {
         window.alert("You have lost your robot in battle! Gamer Over!");
         break;
     }
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
 }
 
 //fight();
